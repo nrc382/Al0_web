231ef0e0b685bd63cf5d167ae7c0bf3963e51f59
nrc382 <gglnrc@me.com>
2023-05-05 22:57:56 +0200

    test

---
+++ server/server_express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const configurazione = require("./statics/configurazione.json");
+
+const app = express();
+const port = configurazione.porta_server;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public/statici')));
+
+// Homepage
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Avvia il server
+app.listen(port, function() {
+  console.log(`Server attivo sulla porta ${port}`);
+});
